0e2a5c071c671a2cf031d2995a366532ec3f257c
Anthony Dcosta <adcosta2000@gmail.com>
2022-08-05 23:26:50 -0400

    added gallary and props

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,23 +1,15 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {capitalizeFirstLetter} from '../../utils/helpers';
 
-function Nav(){
-   const categories = [
-      {
-        name: "commercial",
-        description:
-          "Photos of grocery stores, food trucks, and other commercial projects",
-      },
-      { name: "portraits", description: "Portraits of people in my life" },
-      { name: "food", description: "Delicious delicacies" },
-      {
-        name: "landscape",
-        description: "Fields, farmhouses, waterfalls, and the beauty of nature",
-      },
-    ];
-    // function for eventListener in nav links
-    function categorySelected(name){
-      console.log(`${name} clicked`);
-    }
+function Nav(props){
+   const {
+      categories = [],
+      setCurrentCategory,
+      currentCategory,
+    } = props;
+    useEffect(()=>{
+      document.title = capitalizeFirstLetter(currentCategory.name);
+    }, [currentCategory]);
    return (
       <header className='flex-row px-1'>
           <h2>
@@ -36,10 +28,10 @@ function Nav(){
                   <span>Contact</span>
                </li>
                {categories.map((category) => (
-                  <li className='mx-1'
+                  <li className={`mx-1 ${currentCategory.name === category.name && 'navActive'}`}
                   key={category.name}>
-                     <span onClick={() => categorySelected(category.name)}>
-                        {category.name}
+                     <span onClick={() => {setCurrentCategory(category)}}>
+                        {capitalizeFirstLetter(category.name)}
                      </span>
                   </li>
                ))}
