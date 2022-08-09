e4352c565dd23900289d068fbc513f9fdb3d586f
Anthony Dcosta <adcosta2000@gmail.com>
2022-08-09 13:20:21 -0400

    added conditional rendering for the components

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -6,6 +6,8 @@ function Nav(props){
       categories = [],
       setCurrentCategory,
       currentCategory,
+      contactSelected,
+      setContactSelected
     } = props;
     useEffect(()=>{
       document.title = capitalizeFirstLetter(currentCategory.name);
@@ -20,17 +22,20 @@ function Nav(props){
          <nav>
             <ul className='flex-row'>
                <li className='mx-2'>
-                  <a href='#about' data-testid='about'>
+                  <a href='#about' data-testid='about' onClick={() => setContactSelected(false)}>
                      About me
                   </a>
                </li>
-               <li>
-                  <span>Contact</span>
+               <li className={`mx-2 ${contactSelected && 'navActive'}`}>
+                  <span onClick={() => setContactSelected(false)}>Contact</span>
                </li>
                {categories.map((category) => (
-                  <li className={`mx-1 ${currentCategory.name === category.name && 'navActive'}`}
+                  <li className={`mx-1 ${currentCategory.name === category.name && !contactSelected && 'navActive'}`}
                   key={category.name}>
-                     <span onClick={() => {setCurrentCategory(category)}}>
+                     <span onClick={() => {
+                        setCurrentCategory(category);
+                        setContactSelected(false);
+                     }}>
                         {capitalizeFirstLetter(category.name)}
                      </span>
                   </li>
